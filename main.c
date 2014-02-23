8cf2cfa6cfea38b811eaad2d1ca0708adc852f23
Roman Hudec <black3r@klikni.cz>
2014-02-23 01:49:52 +0100

    Basic CLI functionality (set BootNext)

--- main.c
+++ main.c
@@ -5,13 +5,21 @@
 int main()
 {    
     uint16_t *data;
-    int l, i;
+    int l, i, c;
     data = NULL;
     efi_init();
     l = boot_entry_list(&data);
     for (i = 0; i < l; i++) {
-        printf("%s\n", boot_entry_name(data[i]));
+        printf("%d: %s\n", i, boot_entry_name(data[i]));
     }
+    printf("Input your choice: ");
+    scanf("%d", &c);
+    if (c < 0 || c >= l) {
+        printf("ERROR: Invalid choice\n");
+        exit(-1);
+    }
+    set_boot_next(data[c]);
+    printf("Set next boot target to: %s\n", boot_entry_name(data[c]));
 
     return 0;
 }
